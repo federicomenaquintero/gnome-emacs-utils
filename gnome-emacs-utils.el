6a8c9a2d2a8bb375c05a81c59ee64b720dc6fcc5
Federico Mena Quintero <federico@gnome.org>
2013-03-01 13:32:04 -0600

    Tell the user if something goes wrong while loading the Mallard schema
    Signed-off-by: Federico Mena Quintero <federico@gnome.org>

--- gnome-emacs-utils.el
+++ gnome-emacs-utils.el
@@ -30,9 +30,30 @@
 ;;    that nxml-mode did not find the mallard.rnc file on your system.  Check that the
 ;;    path for mallard.rnc is correct in gnome-emacs-utils/mallard-locating-schema.xml
 
+;; FIXME: this has an absolute pathname.  How should we locate this file?
+(defvar gnome/mallard-locating-schema "/home/federico/src/gnome-emacs-utils/mallard-locating-schema.xml")
+
 (add-to-list 'auto-mode-alist '("\\.page\\'" . xml-mode))
 
 (require 'nxml-mode)
 
-;; FIXME: this has an absolute pathname.  How should we locate this file?
-(push "/home/federico/src/gnome-emacs-utils/mallard-locating-schema.xml" rng-schema-locating-files)
+(push gnome/mallard-locating-schema rng-schema-locating-files)
+
+;; We advice nxml-mode with our checking code, instead of putting
+;; something in nxml-mode-hook, as the hook will not run if an error
+;; happens while the nxml-mode function is being run.  For example,
+;; a hook would not run if nxml-mode fails to open the mallard.rnc file.
+
+(defadvice nxml-mode (after gnome/check-mallard-schema-is-being-used protect activate)
+  "After nxml-mode has been activated, checks if the file in the buffer
+is a .page file (i.e. a Mallard document).  If so, this funtion checks that
+the RNG/RNC schema file for Mallard is indeed being used, and tells the
+user about it.  This is to ensure that Mallard's schema files and
+gnome-emacs-utils in general are installed correctly."
+  (interactive)
+  (when (string-match "\\.page$" buffer-file-name)
+    (if (and rng-current-schema-file-name
+	     (string-match "mallard.rnc$" rng-current-schema-file-name))
+	(rng-what-schema) ;; This will print "Using mallard.rnc" on the minibuffer, to reassure the user
+      (message "Mallard schema is not loaded; please fix gnome/mallard-locating-schema in gnome-emacs-utils.el and ensure the Mallard schema is installed")
+      (sit-for 2))))
